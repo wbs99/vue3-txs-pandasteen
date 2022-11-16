6cca493505173d507477389c3aef61af97983b25
dingtalk_xsgrpl <1134954328@qq.com>
2022-11-16 17:16:07 +0800

    Icon name required

--- src/components/FloatButton.tsx
+++ src/components/FloatButton.tsx
@@ -1,11 +1,20 @@
-import { defineComponent } from 'vue';
+import { defineComponent, PropType } from 'vue';
 import s from './FloatButton.module.scss';
-import { Icon } from './Icon';
+import { Icon, IconName } from './Icon';
 export const FloatButton = defineComponent({
+  props: {
+    iconName: {
+      type: String as PropType<IconName>,
+      required: true
+    }
+  },
+
   setup: (props, context) => {
+    const { iconName } = props
+
     return () => (
       <div class={s.floatButton}>
-        <Icon name="add" class={s.icon} />
+        <Icon name={iconName} class={s.icon} />
       </div>
     )
   }
--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,11 +1,13 @@
 import { defineComponent, PropType } from 'vue';
 import s from './Icon.module.scss'
 
+export type IconName = 'add' | 'welcome1' | 'welcome2' | 'welcome3' | 'panda' | 'welcome4'
 
 export const Icon = defineComponent({
   props: {
     name: {
-      type: String as PropType<'add' | 'welcome1' | 'welcome2' | 'welcome3' | 'panda' | 'welcome4'>
+      type: String as PropType<IconName>,
+      required: true
     }
   },
   setup: (props, context) => {
