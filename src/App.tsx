915a7c5539483353a5406831f39d8bd4e89dcfd6
dingtalk_xsgrpl <1134954328@qq.com>
2022-11-16 09:28:22 +0800

    使用 TSX 代替 template 语法

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { defineComponent, ref } from "vue"
+
+export const App = defineComponent({
+  setup() {
+    const count = ref(0)
+    const onClick = () => {
+      count.value += 1
+    }
+    return () => (
+      <>
+        <div>{count.value}</div>
+        <button onClick={onClick}>+1</button>
+      </>
+    )
+  },
+})
