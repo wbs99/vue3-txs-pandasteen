574a0a350bd632927b5fc13cd2c8fb62f00c721a
dingtalk_xsgrpl <1134954328@qq.com>
2022-11-16 17:12:19 +0800

    实现 FloatButton 组件

---
+++ src/components/FloatButton.tsx
@@ -0,0 +1,12 @@
+import { defineComponent } from 'vue';
+import s from './FloatButton.module.scss';
+import { Icon } from './Icon';
+export const FloatButton = defineComponent({
+  setup: (props, context) => {
+    return () => (
+      <div class={s.floatButton}>
+        <Icon name="add" class={s.icon} />
+      </div>
+    )
+  }
+})
--- src/pages/StartPage.tsx
+++ src/pages/StartPage.tsx
@@ -1,5 +1,6 @@
 import { defineComponent } from 'vue';
 import { Button } from '../components/Button';
+import { FloatButton } from '../components/FloatButton';
 import s from './StartPage.module.scss'
 
 export const StartPage = defineComponent({
@@ -12,6 +13,7 @@ export const StartPage = defineComponent({
         <div class={s.button_wrapper}>
           <Button class={s.button} onClick={onClick}>测试</Button>
         </div>
+        <FloatButton />
       </div>
     )
   }
