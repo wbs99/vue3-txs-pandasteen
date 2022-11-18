9171714d644a33d312a92b1dd5ab57abb22ae1fd
dingtalk_xsgrpl <1134954328@qq.com>
2022-11-18 10:34:08 +0800

    将 time 函数重构成 Time 类

--- src/components/InputPad.tsx
+++ src/components/InputPad.tsx
@@ -1,17 +1,11 @@
 import { defineComponent, PropType, ref } from 'vue';
 import s from './InputPad.module.scss';
-import { DatetimePicker, NumberKeyboard, Popup } from 'vant';
+import { DatetimePicker, Popup } from 'vant';
 import { Icon } from './Icon';
-import { time } from '../shared/time';
+import { Time } from '../shared/time';
 export const InputPad = defineComponent({
-  props: {
-    name: {
-      type: String as PropType<string>
-    }
-  },
   setup: (props, context) => {
-    const now = new Date()
-    const refDate = ref<Date>(now)
+    const refDate = ref<Date>(new Date())
     const appendText = (n: number | string) => {
       const nString = n.toString()
       const dotIndex = refAmount.value.indexOf('.')
@@ -63,7 +57,7 @@ export const InputPad = defineComponent({
         <span class={s.date}>
           <Icon name="date" class={s.icon} />
           <span>
-            <span onClick={showDatePicker}>{time(refDate.value).format()}</span>
+            <span onClick={showDatePicker}>{new Time(refDate.value).format()}</span>
             <Popup position='bottom' v-model:show={refDatePickerVisible.value}>
               <DatetimePicker value={refDate.value} type="date" title="选择年月日"
                 onConfirm={setDate} onCancel={hideDatePicker}
